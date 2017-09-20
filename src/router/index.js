724d92f848305c45057b2265ad6c1dd5b7e58d52
DESKTOP-867U409\Hank <stronghank@gmail.com>
2017-09-21 01:40:31 +0800

    [TBD] Start muse-ui dev, to be done

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import List from '@/components/List'
 import Detail from '@/components/Detail'
-
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+Vue.use(MuseUI)
 Vue.use(Router)
 
 export default new Router({
